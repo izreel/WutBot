a34db1afc1fafb58c24325f63d7109ede202736b
izreel <israpere@gmail.com>
2020-05-07 16:38:17 -0500

    adding method to stream audio from youtube videos to voice channel

--- WutBot.py
+++ WutBot.py
@@ -1,11 +1,23 @@
 import discord
-from discord.ext import commands
 import json
+import youtube_dl
 
 with open('key.config') as json_file:
     data = json.load(json_file)
 
 client = discord.Client()
+
+options = {
+    # 'download_archive' : 'Download_Archive',
+    'outtmpl' : '%(display_id)s.%(ext)s',
+    # 'format' : 'bestaudio/best',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality' : '5'
+    }]
+}
+
 
 @client.event
 async def on_ready():
@@ -27,5 +39,18 @@ async def on_message(message):
     if message.content.startswith('$leave') and client.voice_clients:
         await client.voice_clients[0].disconnect()
 
+    if message.content.startswith('$play'):
+        video_url = message.content.split(' ')[1]
+        audio_file = video_url.split('=')[1] + '.mp3'
+
+        with youtube_dl.YoutubeDL(options) as ydl:
+            ydl.download([video_url])
+        
+        source = await discord.FFmpegOpusAudio.from_probe(audio_file)
+        
+        channel = message.author.voice.channel
+        await channel.connect()
+        
+        client.voice_clients[0].play(source)
 
 client.run(data.get("token"))
