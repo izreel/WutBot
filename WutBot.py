3a849b6a86b28185642d06a57a2b2ad30881c871
izreel <israpere@gmail.com>
2020-05-09 00:40:54 -0500

    added download_archive and printing out title of video

--- WutBot.py
+++ WutBot.py
@@ -2,14 +2,14 @@ import discord
 import json
 import youtube_dl
 
+
 with open('key.config') as json_file:
     data = json.load(json_file)
 
-client = discord.Client()
-
 download_options = {
-    # 'download_archive' : 'Download_Archive',
-    'outtmpl' : './downloads/%(display_id)s.%(ext)s',
+    'download_archive' : '/mnt/d/downloaded_audio/already_downloaded.txt',
+    'outtmpl' : '/mnt/d/downloaded_audio/%(display_id)s.%(ext)s',
+    'format': 'bestaudio/best',
     'postprocessors': [{
         'key': 'FFmpegExtractAudio',
         'preferredcodec': 'mp3',
@@ -17,13 +17,20 @@ download_options = {
     }]
 }
 
+client = discord.Client()
+audio_queue = []
+
 def download_audio(url):
-    audio_file =  './downloads/{}.mp3'.format(url.split('=')[1])
+    audio_file =  '/mnt/d/downloaded_audio/{}.mp3'.format(url.split('=')[1])
 
     with youtube_dl.YoutubeDL(download_options) as ydl:
         ydl.download([url])
-    
+        print(ydl.extract_info(url, download=False)['title'])
     return audio_file
+
+
+def play_audio():
+    client.voice_clients[0].play(audio_queue[0])
 
 @client.event
 async def on_ready():
@@ -48,13 +55,15 @@ async def on_message(message):
     if message.content.startswith('$play'):
         video_url = message.content.split(' ')[1]
         audio_source = await discord.FFmpegOpusAudio.from_probe(download_audio(video_url))
+        audio_queue.append(audio_source)
         
         if not client.voice_clients:
             channel = message.author.voice.channel
             await channel.connect()
 
-        client.voice_clients[0].play(audio_source)
-        
+        if not client.voice_clients[0].is_playing():
+            client.voice_clients[0].play(audio_source)
+            
     if message.content.startswith('$pause') and client.voice_clients[0].is_playing():
         client.voice_clients[0].pause()
 
